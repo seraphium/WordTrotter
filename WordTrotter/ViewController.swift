2924394ed86bea440f1be5da85a5861c49d09616
Jackie Zhang <viperking@163.com>
2016-02-25 21:40:18 +0800

    Initial Commit

---
+++ WordTrotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordTrotter
+//
+//  Created by Jackie Zhang on 16/2/25.
+//  Copyright © 2016年 Jackie Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
