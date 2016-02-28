0f6920c1912c05bace070b26e77410ffcc33b905
Jackie Zhang <viperking@163.com>
2016-02-28 14:54:00 +0800

    added resource files for localization

--- WordTrotter/MapViewController.swift
+++ WordTrotter/MapViewController.swift
@@ -16,7 +16,12 @@ class MapViewController : UIViewController {
         mapView = MKMapView()
         view = mapView
         
-        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        let standardString = NSLocalizedString("Standard", comment: "Standard map view")
+        let hybridString = NSLocalizedString("Hybrid", comment: "Hybrid map view")
+        let satelliteString = NSLocalizedString("Satellite", comment: "Satellite map view")
+
+        let segmentedControl = UISegmentedControl(items: [standardString, hybridString, satelliteString])
+        
         segmentedControl.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
         segmentedControl.selectedSegmentIndex = 0;
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
